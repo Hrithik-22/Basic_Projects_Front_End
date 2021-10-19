548d6849cc64a6ad36bf1d00b2ea3c5f63f904b5
Hrithik-22 <71004711+Hrithik-22@users.noreply.github.com>
2021-10-19 22:44:55 +0530

    Add files via upload

---
+++ Sound Board/app.js
@@ -0,0 +1,28 @@
+const sounds=['applause','boo','gasp','tada','victory','wrong'];
+
+sounds.forEach(sound=>{
+
+    const btn=document.createElement('button');
+    btn.classList.add('btn')
+
+    btn.innerText=sound
+
+    btn.addEventListener('click',()=>{
+        soundStop();
+        
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+
+    
+})
+
+function soundStop(){
+    sounds.forEach(sound=>{
+        const song=document.getElementById(sound);
+
+        song.pause()
+        song.currentTime=0;
+    })
+}
